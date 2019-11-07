eab714087057bb8b33597fc06704dad8b9011bae
Marco Guerrero <ing._guerrero@hotmail.com>
2019-11-07 15:40:52 -0600

    Modifiacion de app y config de pantalla

--- RegistrodeEntradas/app/src/main/java/com/example/registrodeentradas/MainActivity.java
+++ RegistrodeEntradas/app/src/main/java/com/example/registrodeentradas/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);6
+        setContentView(R.layout.activity_main);
 
 
     }
---
+++ RegistrodeEntradas/app/src/main/java/com/example/registrodeentradas/index.java
@@ -0,0 +1,4 @@
+package com.example.registrodeentradas;
+
+public class index {
+}
